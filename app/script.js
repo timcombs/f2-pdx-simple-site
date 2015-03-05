87865b7bf88838f12497249e6226f455e38f7754
Stephanie Lingwood <stephanielingwood@gmail.com>
2015-03-05 10:17:40 -0800

    commented out code to be added in during class

--- app/script.js
+++ app/script.js
@@ -1,19 +1,38 @@
 $(function() {
 
   $("button").click(function() {
-    var url = $(this).attr("id");
-
-    $.get(url, function(response) {
-      var resText;
-
-      if (typeof response === "object") {
-        resText = response.setup + ": " + response.punchline;
-      } else {
-        resText = response;
-      }
-
-      $("#ajax-text").text(resText);
-    });
-
+    $("#ajax-text").text("hi");
   });
+
 });
+
+
+
+
+
+
+
+
+
+
+    // $.get("http://localhost:3000/quote", function(response) {
+    //   $("#ajax-text").text(response);
+    // });
+
+
+
+    // var url = $(this).attr("id");
+
+    // $.get(url, function(response) {
+    //   var resText;
+
+    //   if (typeof response === "object") {
+    //     resText = response.setup + ": " + response.punchline;
+    //   } else {
+    //     resText = response;
+    //   }
+
+    //   $("#ajax-text").text(resText);
+    // });
+
+
