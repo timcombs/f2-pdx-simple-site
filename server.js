ac395940011f963cbc494ab337153dc20eec4e54
Stephanie Lingwood <stephanielingwood@gmail.com>
2015-03-05 23:05:54 -0800

    fixed jscs errors

--- server.js
+++ server.js
@@ -26,41 +26,20 @@ var jokes = [
     punchline: "Ten Tickles" }
 ];
 
-
-// function piglatinify(word) {
-//   var letters;
-//   var changedWord;
-//   var vowelHash = {a: 1, e: 1, i: 1, o:1, u:1, A: 1, E: 1, I: 1, O: 1, U: 1};
-//   var wordArray = word.split('');
-//   //if it starts with a vowel, just add "-hay" and stop here
-//   if (vowelHash.hasOwnProperty(wordArray[0])) {
-//     return word + "-hay";
-//   }
-//   //if it starts with two consonants
-//   if (!vowelHash.hasOwnProperty(wordArray[1])) {
-//     //check to see if it starts with three consonants
-//     if (!vowelHash.hasOwnProperty(wordArray[2])) {
-//       //take off the first three consonants and append them to the end of the word, plus ay
-//       letters = wordArray.splice(0, 3)
-//       wordArray[0] = wordArray[0].toUpperCase();
-//       letters[0] = letters[0].toLowerCase();
-//       changedWord = wordArray.join("") + "-" + letters.join("") + "ay";
-//       return changedWord;
-//     }
-//     //if it starts with two consonants
-//     letters = wordArray.splice(0, 2)
-//     wordArray[0] = wordArray[0].toUpperCase();
-//     letters[0] = letters[0].toLowerCase();
-//     changedWord = wordArray.join("") + "-" + letters.join("") + "ay";
-//     return changedWord;
-//   }
-//   //otherwise, it starts with one consonant
-//   letters = wordArray.shift();
-//   wordArray[0] = wordArray[0].toUpperCase();
-//   letters = letters.toLowerCase();
-//   changedWord = wordArray.join("") + "-" + letters + "ay";
-//   return changedWord;
-// }
+function piglatinify(word) {
+  var letters;
+  var changedWord;
+  var vowelHash = { a: 1, e: 1, i: 1, o:1, u:1, A: 1, E: 1, I: 1, O: 1, U: 1 };
+  var wordArray = word.split("");
+  //if it starts with a vowel, just add "-hay" and stop here
+  if (vowelHash.hasOwnProperty(wordArray[0])) {
+    return word + "-hay";
+  }
+  //otherwise, it starts with a consonant
+  letters = wordArray.shift();
+  changedWord = wordArray.join("") + "-" + letters + "ay";
+  return changedWord;
+}
 
 app.get("/joke", function(req, res) {
   var randomIndex = Math.floor(Math.random() * jokes.length);
@@ -75,7 +54,7 @@ app.get("/quote", function(req, res) {
 app.post("/piglatin", function(req, res) {
   var firstname = piglatinify(req.body.firstname);
   var lastname = piglatinify(req.body.lastname);
-  var piglatined = {firstname: firstname, lastname: lastname};
+  var piglatined = { firstname: firstname, lastname: lastname };
   res.json(piglatined);
 });
 
