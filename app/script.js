498078cd2343fc540adf6671cb267438535cac63
Stephanie Lingwood <stephanielingwood@gmail.com>
2015-03-05 22:59:14 -0800

    added form, post route, and piglatinify

--- app/script.js
+++ app/script.js
@@ -2,18 +2,29 @@ $(function() {
 
   $("button").click(function() {
     var url = $(this).attr("id");
-
     $.get(url, function(response) {
       var resText;
-
       if (typeof response === "object") {
         resText = response.setup + ": " + response.punchline;
       } else {
         resText = response;
       }
-
       $("#ajax-text").text(resText);
+    });
+  });
+
+  $("#piglatin").on("submit", function(e) {
+    e.preventDefault();
+
+    var firstname = $('input[name=firstname]').val();
+    var lastname = $('input[name=lastname]').val();
+    var name = {firstname: firstname, lastname: lastname};
+
+    $.post("piglatin", name, function(response) {
+      var piglatinified = response.firstname + " " + response.lastname;
+      $("#piglatinified").text(piglatinified);
     });
 
   });
+
 });
--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 var express = require("express");
 var app = express();
+var bodyparser = require("body-parser");
 var port = process.env.PORT || 3000;
+var piglatinify = require("./lib/piglatinify.js");
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({
+extended: true
+}));
 
 app.use(express.static(__dirname + "/app/"));
 
@@ -19,6 +26,42 @@ var jokes = [
     punchline: "Ten Tickles" }
 ];
 
+
+// function piglatinify(word) {
+//   var letters;
+//   var changedWord;
+//   var vowelHash = {a: 1, e: 1, i: 1, o:1, u:1, A: 1, E: 1, I: 1, O: 1, U: 1};
+//   var wordArray = word.split('');
+//   //if it starts with a vowel, just add "-hay" and stop here
+//   if (vowelHash.hasOwnProperty(wordArray[0])) {
+//     return word + "-hay";
+//   }
+//   //if it starts with two consonants
+//   if (!vowelHash.hasOwnProperty(wordArray[1])) {
+//     //check to see if it starts with three consonants
+//     if (!vowelHash.hasOwnProperty(wordArray[2])) {
+//       //take off the first three consonants and append them to the end of the word, plus ay
+//       letters = wordArray.splice(0, 3)
+//       wordArray[0] = wordArray[0].toUpperCase();
+//       letters[0] = letters[0].toLowerCase();
+//       changedWord = wordArray.join("") + "-" + letters.join("") + "ay";
+//       return changedWord;
+//     }
+//     //if it starts with two consonants
+//     letters = wordArray.splice(0, 2)
+//     wordArray[0] = wordArray[0].toUpperCase();
+//     letters[0] = letters[0].toLowerCase();
+//     changedWord = wordArray.join("") + "-" + letters.join("") + "ay";
+//     return changedWord;
+//   }
+//   //otherwise, it starts with one consonant
+//   letters = wordArray.shift();
+//   wordArray[0] = wordArray[0].toUpperCase();
+//   letters = letters.toLowerCase();
+//   changedWord = wordArray.join("") + "-" + letters + "ay";
+//   return changedWord;
+// }
+
 app.get("/joke", function(req, res) {
   var randomIndex = Math.floor(Math.random() * jokes.length);
   res.json(jokes[randomIndex]);
@@ -27,6 +70,13 @@ app.get("/joke", function(req, res) {
 app.get("/quote", function(req, res) {
   var randomIndex = Math.floor(Math.random() * quotes.length);
   res.send(quotes[randomIndex]);
+});
+
+app.post("/piglatin", function(req, res) {
+  var firstname = piglatinify(req.body.firstname);
+  var lastname = piglatinify(req.body.lastname);
+  var piglatined = {firstname: firstname, lastname: lastname};
+  res.json(piglatined);
 });
 
 app.get("/", function(req, res) {
