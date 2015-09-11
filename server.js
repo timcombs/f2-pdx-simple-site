30cd3444f745945d97e87660ae461456b19b9f40
Tim Combs <tim00combs@gmail.com>
2015-09-10 17:36:50 -0700

    4th committ for final project - fixed JSON notation, added more endpoints.

--- server.js
+++ server.js
@@ -2,22 +2,22 @@ var express = require("express");
 var app = express();
 var port = process.env.PORT || 3000;
 
-var course = {
-  level: "201",
+var course: {
+  level: 201,
   name: "Foundations II JavaScript",
   lectures: [
-    {topic: "Paperwork, Setup & JavaScript Basics (Part 1)"},
-    {topic: "JavaScript Basics (Part 2) & Intro to Node"},
-    {topic: "Array Methods, Functions & Scope/Hoisting"},
-    {topic: "Object-Oriented Programming in JavaScript and Classes/Inheritance"},
-    {topic: "JavaScript in the Browser & Intro to jQuery"},
-    {topic: "Building a Server & Using Ajax"},
-    {topic: "Build & Deploy an App"},
-    {topic: "Functional Programming with lodash"}
+    {"topic": "Paperwork, Setup & JavaScript Basics (Part 1)"},
+    {"topic": "JavaScript Basics (Part 2) & Intro to Node"},
+    {"topic": "Array Methods, Functions & Scope/Hoisting"},
+    {"topic": "Object-Oriented Programming in JavaScript and Classes/Inheritance"},
+    {"topic": "JavaScript in the Browser & Intro to jQuery"},
+    {"topic": "Building a Server & Using Ajax"},
+    {"topic": "Build & Deploy an App"},
+    {"topic": "Functional Programming with lodash"}
   ],
   labs: [
-    {topic: "A Trip to the Zoo"},
-    {topic: "Loopy Sci-Fi"},
+    {"topic": "A Trip to the Zoo"},
+    {"topic": "Loopy Sci-Fi"}
   ]
 };
 
@@ -29,7 +29,25 @@ app.get("/", function(req, res) {
   res.send("hello universe!");
 });
 
-app.get("/coursework", function(req, res) {
-  var randomIndex = Math.floor(Math.random()*course.lectures.length);
-  res.send(course.lectures[randomIndex].topic);
+app.get("/lectures", function(req, res) {
+  var randomIndexA = Math.floor(Math.random()*course.lectures.length);
+  res.send(course.lectures[randomIndexA].topic);
 });
+
+app.get("/level", function(req, res) {
+  res.send(course.level);
+});
+
+app.get("/name", function(req, res){
+  res.send(course.name);
+});
+
+app.get("/labs", function(req,res) {
+  var randomIndexB = Math.floor(Math.random()*course.labs.length);
+  res.send(course.labs[randomIndexB].topic);
+});
+
+
+
+
+
